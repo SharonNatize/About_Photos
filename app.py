fb44fad408b0f34b0032217d8519621702c60cfe
Sharon Natize <sharon.natividad@unmsm.edu.pe>
2025-01-16 15:08:40 -0500

    delete fix port

--- app.py
+++ app.py
@@ -2,8 +2,6 @@ import streamlit as st
 import os
 from PIL import Image
 
-PORT = int(os.environ.get("PORT", 8501))  # Render asigna el puerto en la variable de entorno PORT
-st.run(port=PORT)
 # Ruta de la carpeta "assets" donde están las imágenes
 assets_path = 'C:/Users/Sharon/Desktop/Test_fotos/assets'
 
